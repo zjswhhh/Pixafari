895f3ba632640b9b6e5d3a80e452875a68261f16
zjswhhh <zjsw1206@gmail.com>
2018-08-05 03:12:45 -0400

    profile page completed

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,8 @@ const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: '',
-	database: 'pixafari'
+	database: 'pixafari',
+	multipleStatements: true
 });
 
 connection.connect(err => {
@@ -27,8 +28,9 @@ app.get('/', (req, res) => {
 	res.send('Hello from Pixafari server,  go to /trips to see all the trips.')
 });
 
+//--------------------------------//
 //------Unique ID Generator-------//
-
+//--------------------------------//
 var ID = function () {
   	// Math.random should be unique because of its seeding algorithm.
   	// Convert it to base 36 (numbers + letters), and grab the first 9 characters
@@ -36,8 +38,11 @@ var ID = function () {
   	return '_' + Math.random().toString(36).substr(2, 9);
 };
 
-//-------------------------------//
 
+
+//--------------------------------//
+//---------Add New Trip----------//
+//-------------------------------//
 app.get('/trips/add', (req, res) => {
 	const { trip_name, start_date, end_date, budget, destination, trip_type} = req.query;
 	const groupID = ID();
@@ -53,6 +58,10 @@ app.get('/trips/add', (req, res) => {
 	
 });
 
+
+//--------------------------------//
+//---------Show all trips----------//
+//-------------------------------//
 app.get('/trips', (req, res) => {
 	connection.query(SELECT_ALL_TRIPS_QUERY, (err, results) => {
 		if(err) {
@@ -66,6 +75,46 @@ app.get('/trips', (req, res) => {
 	});
 });
 
+
+//--------------------------------//
+//---------Profile Page----------//
+//-------------------------------//
+
+app.get('/profile', (req, res) => {
+	const { userName } = req.query;
+	const SELECT_PROFILE = `drop view t3;
+
+create view t3 as (select groupID, count(*) as total from (
+select member.groupID as groupID, member.userID as userID from 
+(select member.groupID as groupID from member 
+join user on user.userID = member.userID 
+where user.userName = '${userName}') AS t1
+join member on member.groupID = t1.groupID) as t2
+group by groupID);
+
+select trip.trip_name as trip_name, t3.groupID as groupID, COALESCE(votes,0) as votes, total  from (
+select t3.groupID as groupID, count(*) as votes from t3
+join vote on vote.groupID = t3.groupID
+group by groupID) as t4
+right join t3 on t3.groupID = t4.groupID
+left join trip on trip.groupID = t3.groupID; 
+`;
+	connection.query(SELECT_PROFILE, (err, results) => {
+		if(err) {
+			return res.send(err)
+		} 
+		else {
+			return res.json({
+				data: results[2]
+			})
+		}
+	});
+});
+
+
+//--------------------------------//
+//---------Result Page----------//
+//-------------------------------//
 app.get('/results', (req, res) => {
 	const { trip_name } = req.query;
 	const SELECT_RESULT = `select * from trip where trip_name = '${trip_name}'`;
--- frontend/src/routes/Profile.js
+++ frontend/src/routes/Profile.js
@@ -9,31 +9,83 @@ class Profile extends Component {
     super();
   }
 
+  state = {
+    profiles : [],
+    profile: {
+      groupID: '',
+      trip_name: '',
+      votes: '',
+      total: ''
+    }
+  }
+
+  getProfile = _ => {
+      const { profile } = this.state;
+      fetch(`http://localhost:4000/profile?userName=${profile.userName}`)
+      .then(response => response.json())
+      .then(response => this.setState({ profiles: response.data }))
+      .catch(err => console.error(err))
+  }
+
+  renderProfile = ({ groupID, trip_name, votes, total }) => 
+  <div className="trip-container row">
+            <div className="trip-left col-sm-4">
+                   <div className="trip-image">
+                        <img
+                          src="https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/1200/600/90/secure.parksandresorts.wdpromedia.com/media/abd/refresh/north-america/san-francisco-tours/adventures-by-disney-north-america-san-francisco-long-weekend-hero-01-golden-gate-bridge.jpg"
+                          alt="San Francisco"
+                          width="110%"
+                          height="160em"
+                        />
+                  </div>
+            </div>
+
+            <div className="trip-center col-sm-5">
+                  <div className="trip-title">
+                      <h2>{trip_name}</h2>
+                  </div>
+
+                  <div className="trip-vote">
+                      <span><i class="fa fa-check"></i> {votes} out of {total} member voted</span>
+                 </div>
+
+                 <div className="trip-result">
+                      <span><i class="fa fa-eye"></i><a href="/result"> See result</a></span>
+                 </div>
+             </div>
+
+             <div className="trip-right col-sm-3">
+                  <div className="trip-edit">
+                    edit  <i class="fa fa-pencil"></i>
+                  </div>
+
+                  <div className="trip-reminder">
+                    <span>send reminder  <i class="fa fa-envelope"></i> <span className="glyphicon glyphicon-envelope"></span></span>
+                  </div>
+             </div>
+        </div>;
+
+
   render() {
+    const {profiles, profile} = this.state;
     return (
     <div>
       <Navbar / >
-
-      <div className="profile-container">
-
-      <div className="row profile-row-1 profile-icon">
-
-         <a href="/profile">Angela <i className="fa fa-user"></i></a>
-
+        User Name: 
+         <input
+           value={profile.userName}
+           onChange={e => this.setState({ profile: {...profile, userName: e.target.value}})}
+          />
+          <button
+            type="button"
+            class="btn btn-primary btn-lg"
+            onClick={this.getProfile}
+          >Get Profile
+          </button>
+      
+      <div>
+       {profiles.map(this.renderProfile)} 
       </div>
-
-      <div className="row profile-row-2">
-
-        <UserTrip/>
-           <div className=""></div>
-
-      </div>
-
-      <div className=""></div>
-
-      </div> {/* Profile Container */}
-
-
 
     </div>
     );
